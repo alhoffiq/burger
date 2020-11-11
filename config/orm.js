433686bb2b04805db829e6afbc6bcb06a24daec4
alhoff <andrew.hoff14@yahoo.com>
2020-11-10 19:50:00 -0700

    setup inital db, config, and model

---
+++ config/orm.js
@@ -0,0 +1,48 @@
+const connection = require("../config/connection.js");
+
+const orm = {
+  selectAll: function (tableInput, cb) {
+    const queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  insertOne: function (table, cols, vals, cb) {
+    let queryString = "INSERT INTO " + table;
+
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += "?";
+    queryString += ") ";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function (err, result) {
+      if (err) throw err;
+
+      cb(result);
+    });
+  },
+  updateOne: function (table, objColVals, condition, cb) {
+    let queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objColVals
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function (err, result) {
+      if (err) throw err;
+
+      cb(result);
+    });
+  },
+};
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers (burger_name)
+VALUES ("Double cheeseburger"),("Bacon burger"),("Veggie burger")
