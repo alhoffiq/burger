660ff43b962e9d83128b454590d20ff4140f72df
alhoff <andrew.hoff14@yahoo.com>
2020-11-12 00:01:04 -0700

    changed connection to createPool from createConnection to prevent heroku from closing connection to db after 1 minute of timeout

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: "us-cdbr-east-02.cleardb.com",
   port: 3306,
   user: "b45442c7301719",
@@ -8,12 +8,12 @@ const connection = mysql.createConnection({
   database: "heroku_0b1987c772a7eca"
 });
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
 module.exports = connection;
